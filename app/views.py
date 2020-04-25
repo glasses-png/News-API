84f695e9289eb0796b4b2477faaae7f743030fae
glasses-png <cchelangat16@gmail.com>
2020-04-25 12:37:59 +0300

    install flask bootstrap

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from ..models import Sources
 
 # Views
 @app.route('/')
@@ -8,5 +9,10 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    
-    return render_template('index.html')
+    sources = get_sources('breaking news')
+    sports_sources = get_sources('sports')
+    technology_sources = get_sources('technology')
+    entertainment_sources = get_sources('entertainment')
+    title = "News Chap Chap"
+
+    return render_template('index.html', title = title, sources= sources, sports_sources = sports_sources, technology_sources = technology_sources,entertainment_sources = entertainment_sources)
