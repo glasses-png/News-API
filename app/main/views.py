3f4495a6e8e349f5f2407b5ae0ea7cc0e100efb7
glasses-png <cchelangat16@gmail.com>
2020-04-26 13:43:17 +0300

    create tests folder

--- app/main/views.py
+++ app/main/views.py
@@ -6,24 +6,25 @@ from ..models import Sources
 # Views
 @main.route('/')
 def index():
+	'''
+	view root page function that returns the index the page and its data
+	'''
+	sources = get_sources('breaking news')
+	sports_sources = get_sources('sports')
+	technology_sources = get_sources('technology')
+	entertainment_sources = get_sources('entertainment')
+	title = "News Chap Chap"
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-    news_sources = get_sources('breaking news')
-    sports_sources = get_sources('sports')
-    technology_sources = get_sources('technology')
-    entertainment_sources = get_sources('entertainment')
-    title = "News Chap Chap"
-
-    return render_template('index.html', title = title, sources= sources, sports_sources = sports_sources, technology_sources = technology_sources,entertainment_sources = entertainment_sources)
+	return render_template('index.html',title = title, sources = sources,sports_sources = sports_sources,technology_sources = technology_sources,entertainment_sources = entertainment_sources)
 
 @main.route('/sources/<id>')
 def articles(id):
-    '''
-    View articles page
-    '''
-    articles = get_articles(id)
-    title =f'NCC | {id}'
+	'''
+	view articles page
+	'''
+	articles = get_articles(id)
+	title = f'NCC | {id}'
 
-    return render_template('articles.html',title= title,articles = articles)
+	return render_template('articles.html',title= title,articles = articles)
+
+
